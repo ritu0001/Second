dd21fd8d0eafca76e986ec80d3117b2e0c8b0902
ritu0001 <91780950+ritu0001@users.noreply.github.com>
2023-01-27 19:43:20 +0530

    Add files via upload

---
+++ login system/admin_page.php
@@ -0,0 +1,43 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['admin_name'])){
+   header('location:login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>admin page</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+   <style>
+    body{ background-image: url('greenww.jpg');
+     background-repeat: no-repeat;
+   background-attachment: fixed;  
+   background-size: cover;}
+  </style>
+</head>
+<body>
+   
+<div class="container">
+
+<div class="content">
+     <br> <p>To return to home page click !!</p>
+      <a href="New Text Document.html" class="btn">Home</a>
+   </div>
+
+
+</div>
+
+</body>
+</html>
